1a1618abe9d160154f0a94a494f92bbaa89ad2e5
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 04:59:26 -0400

    Allow empty commits in publish.sh

--- publish.sh
+++ publish.sh
@@ -8,6 +8,6 @@ git checkout gh-pages
 cp output/*.html .
 cp output/results.json .
 git add *.html results.json
-git commit -m 'Update generated files.'
+git commit -m 'Update generated files.' --allow-empty
 git push
 git checkout -
