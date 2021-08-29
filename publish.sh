cbeb718c0920517765bc1089ea027c83bcef1f64
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 17:50:20 -0400

    Use -x for logging in publish.sh

--- publish.sh
+++ publish.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
-set -eu
+set -eux
 
-echo 'Inspecting output...'
 [[ -f output/extracted-styles.json ]]
 [[ -f output/scp-001.html ]]
 
-echo 'Checking out branch...'
 git checkout gh-pages
-
-echo 'Copying files...'
-cp output/*.html .
-cp output/results.json .
-
-echo 'Committing files...'
-git add *.html results.json
+cp -a output/pages output/results.json .
+git add pages/ results.json
 git commit -m 'Update generated files.' --allow-empty
-
-echo 'Pushing files...'
 git push
 git checkout -
-
-echo 'Done.'
