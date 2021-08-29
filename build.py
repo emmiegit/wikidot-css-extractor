93b4879881413ea3ca8a570c607e1b9018d623e6
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 02:35:18 -0400

    Process counts for different kinds of objects.

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 
 import json
 import os
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from datetime import datetime
 
 import jinja2
@@ -10,7 +10,9 @@ import jinja2
 STYLES_FILENAME = 'output/extracted-styles.json'
 OUTPUT_HTML = 'index.html'
 
-def build_html(pages, styles):
+CountedItems = namedtuple('CountedItems', ('module_styles', 'inline_styles', 'classes'))
+
+def build_html(pages, counts):
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader('.'),
         autoescape=True,
@@ -31,11 +33,18 @@ def build_html(pages, styles):
             slug=slug,
             title=page['title'],
             source=page['source'],
-            styles=page['styles'],
+            module_styles=page['moduleStyles'],
+            inline_styles=page['inlineStyles'],
+            classes=page['classes'],
         )
 
     print("Generating index...")
-    html_pages['index'] = index_template.render(pages=pages, styles=styles)
+    html_pages['index'] = index_template.render(
+        pages=pages,
+        module_styles=counts.module_styles,
+        inline_styles=counts.inline_styles,
+        classes=counts.classes,
+    )
     return html_pages
 
 def write_html(html_pages):
@@ -78,22 +87,39 @@ def load_pages(path):
 
     return pages
 
-def deduplicate_styles(pages):
-    styles_count = defaultdict(int)
+def deduplicate_items(pages):
+    module_styles_count = defaultdict(int)
+    inline_styles_count = defaultdict(int)
+    classes_count = defaultdict(int)
+
     for page in pages:
-        for style in page['styles']:
-            styles_count[style] += 1
+        for style in page['moduleStyles']:
+            module_styles_count[style] += 1
 
-    styles = []
-    for style, count in styles_count.items():
-        styles.append((style, count))
+        for style in page['inlineStyles']:
+            inline_styles_count[style] += 1
 
-    styles.sort(key=lambda item: item[1])
-    styles.reverse()
-    return styles
+        for klass in page['classes']:
+            classes_count[klass] += 1
+
+    def convert(counts):
+        items = [(item, count) for item, count in counts.items()]
+        items.sort(key=lambda item: item[1])
+        items.reverse()
+        return items
+
+    module_styles = convert(module_styles_count)
+    inline_styles = convert(inline_styles_count)
+    classes = convert(classes_count)
+
+    return CountedItems(
+        module_styles=module_styles,
+        inline_styles=inline_styles,
+        classes=classes,
+    )
 
 if __name__ == '__main__':
     pages = load_pages(STYLES_FILENAME)
-    styles = deduplicate_styles(pages)
-    generated_html = build_html(pages, styles)
+    counts = deduplicate_items(pages)
+    generated_html = build_html(pages, counts)
     write_html(generated_html)
