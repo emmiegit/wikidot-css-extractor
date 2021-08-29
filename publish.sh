025041394c2105edb6b83e2079e70c9bf071709c
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 05:06:55 -0400

    Add logging to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
+echo 'Inspecting output...'
 [[ -f output/extracted-styles.json ]]
 [[ -f output/scp-001.html ]]
 
+echo 'Checking out branch...'
 git checkout gh-pages
+
+echo 'Copying files...'
 cp output/*.html .
 cp output/results.json .
+
+echo 'Committing files...'
 git add *.html results.json
 git commit -m 'Update generated files.' --allow-empty
+
+echo 'Pushing files...'
 git push
 git checkout -
+
+echo 'Done.'
