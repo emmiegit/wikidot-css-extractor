e39d6a8ce28eef925fa37fdf184b2a903a923f8e
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 01:37:41 -0400

    Use lazy evaluation for inline styles.

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 const BASE_URL = 'https://scp-wiki.wikidot.com/'
 
 const REGEX_MODULE_CSS = /\[\[module +css\]\]\n(.+)\n\[\[\/module\]\]/gmi;
-const REGEX_INLINE_CSS = /style="(.+)"/gi;
+const REGEX_INLINE_CSS = /style="(.+?)"/gi;
 
 const STYLES_FILENAME = 'output/extracted-styles.json';
 
