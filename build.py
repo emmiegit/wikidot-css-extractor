17f8d72d059bfdeab645a3d3244c5d6782241501
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 23:12:28 -0400

    Rename pages_index.j2 -> page-index.j2

--- build.py
+++ build.py
@@ -57,7 +57,7 @@ def build_html(pages, counts):
     inline_styles_template = env.get_template('inline-css.j2')
     includes_template = env.get_template('includes.j2')
     classes_template = env.get_template('classes.j2')
-    pages_index_template = env.get_template('pages_index.j2')
+    page_index_template = env.get_template('page-index.j2')
     index_template = env.get_template('index.j2')
 
     # Build HTML
@@ -82,7 +82,7 @@ def build_html(pages, counts):
     html_pages['inline-css'] = inline_styles_template.render(styles=counts.inline_styles)
     html_pages['includes'] = includes_template.render(includes=counts.includes)
     html_pages['classes'] = classes_template.render(classes=counts.classes)
-    html_pages['pages/index'] = pages_index_template.render(pages=pages)
+    html_pages['pages/index'] = page_index_template.render(pages=pages)
 
     print("Generating index...")
     html_pages['index'] = index_template.render(
