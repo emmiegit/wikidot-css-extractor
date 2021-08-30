d7ee740c166e777d1fc078d2e871e70a74ce2fe6
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 21:38:23 -0400

    Update publish.sh to ensure git checkout doesn't cause issues.

--- publish.sh
+++ publish.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+
+# Setup
+temp_dir="$(mktemp -d)"
+
+function on_exit() {
+	rm -rf "$temp_dir"
+}
+
+# Execution
+set -x
 
 [[ -f output/results.json ]]
 [[ -f output/index.html ]]
 [[ -f output/pages/index.html ]]
 [[ -f output/pages/scp-001.html ]]
 
+cp -a output/*.html output/pages output/results.json "$temp_dir"
 git checkout gh-pages
-cp -a output/*.html output/pages output/results.json .
-git add *.html pages/ results.json
+cp -a "$temp_dir"/* .
+git add .
 git commit -m 'Update generated files.'
 git push
 git checkout -
