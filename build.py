ead66e0a58f54e1e4cc470de4b06282f87ef684a
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 22:07:20 -0400

    Add include_link() helper to utils.j2

--- build.py
+++ build.py
@@ -34,7 +34,7 @@ def get_include_url(include):
     if site is None:
         site = 'scp-wiki'
 
-    return f"https://{site}.wikidot.com/{pagae}"
+    return f"https://{site}.wikidot.com/{page}"
 
 
 def build_html(pages, counts):
