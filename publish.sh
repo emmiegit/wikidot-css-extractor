faedb844a1aa0a70e48e2fc8062c930606c98d75
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-29 20:41:12 -0400

    Fix publish.sh checks.

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
-[[ -f output/extracted-styles.json ]]
-[[ -f output/scp-001.html ]]
+[[ -f output/results.json ]]
+[[ -f output/pages/scp-001.html ]]
 
 git checkout gh-pages
 cp -a output/pages output/results.json .
