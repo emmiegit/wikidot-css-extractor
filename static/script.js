421e171d638dcba18c9e265cbec9d7dc6f14d94e
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-31 00:41:31 -0400

    Add logging on tablesort init.

--- static/script.js
+++ static/script.js
@@ -1,8 +1,9 @@
 // Wait a bit first
-settimeout(100, function() {
+setTimeout(100, function() {
   var element = document.getElementById('page-list');
   if (element !== null) {
     // Actually initialize the table sorting
+    console.info("Initializing tablesort for " + element);
     new Tablesort(element);
   }
 });
