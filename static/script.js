609ac2ae898a41690ce79058fae9ca35c9ff6762
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-31 01:18:08 -0400

    Separately disable notice elements.

--- static/script.js
+++ static/script.js
@@ -1,9 +1,13 @@
 function initTableSort() {
+  // Init table sort
   var tableElement = document.getElementById('page-list');
   new Tablesort(tableElement, { descending: true });
 
-  var noticeElement = document.getElementById('table-sort-notice');
-  noticeElement.remove();
+  // Disable table sort notice
+  var noticeElements = document.getElementsByClassName('table-sort-notice');
+  for (var i = 0; i < noticeElements.length; i++) {
+    noticeElements[i].remove();
+  }
 
-  console.info("Initializing tablesort for " + tableElement);
+  console.info("Initialized tablesort for " + tableElement);
 }
