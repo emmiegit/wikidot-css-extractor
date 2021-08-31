843998b9406268838a54fed0854893e8cecdac49
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-31 00:36:35 -0400

    Delay tablesort initialization a bit.

--- static/script.js
+++ static/script.js
@@ -1,4 +1,8 @@
-var element = document.getElementById('page-list');
-if (element !== null) {
-  new Tablesort(element);
-}
+// Wait a bit first
+settimeout(100, function() {
+  var element = document.getElementById('page-list');
+  if (element !== null) {
+    // Actually initialize the table sorting
+    new Tablesort(element);
+  }
+});
