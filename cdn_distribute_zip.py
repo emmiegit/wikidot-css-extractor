d0a4393783ed23670542a04d6a1384f61a9e8c8c
Emmie Maeda <emmie.maeda@gmail.com>
2023-11-11 01:15:34 -0500

    Add (second) initial version of cdn_distribute_zip.py
    Should've kept the first version rip

---
+++ cdn_distribute_zip.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from glob import iglob
+import os
+import shutil
+
+if __name__ == "__main__":
+    base_directory = os.path.expanduser("~/incoming/cdndiscord")
+    for site in os.listdir(base_directory):
+        if site == "files":
+            continue
+
+        print(f"Opening directory {site}")
+        directory = os.path.join(base_directory, site)
+        for path in iglob(os.path.join(directory, "*.txt")):
+            print(f"Processing file {path}")
+            with open(path) as file:
+                lines = file.read().split("\n")
+
+            for line in lines:
+                if not line:
+                    continue
+
+                url, filename = line.split()
+                source_path = os.path.join(base_directory, "files", filename)
+                dest_path = os.path.join(directory, filename)
+                if not os.path.isfile(dest_path):
+                    print(f"Copying image {filename}")
+                    shutil.copyfile(source_path, dest_path)
